5ddd9d16d63a421f1c52a093b24f4affe0e98f74
Valerii <Kuramov.v@mail.ru>
2023-12-13 13:39:40 +0300

    added a method for checking the element on the page and correctly processed the error information output

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) # initialize the Page Object, pass the driver instance and the url to the constructor
     page.open()  # opening the page
     page.go_to_login_page() # executing the page method — go to the login page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
