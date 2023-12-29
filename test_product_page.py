57be94a16bfff0230a971ccb97f6e378a5ce2b18
Valerii <Kuramov.v@mail.ru>
2023-12-29 11:27:01 +0300

    the tests for the review were labeled, and a mark description file was created

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ link_promo = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-ha
 #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 link_not_promo = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -70,13 +71,13 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     product_page = ProductPage(browser, link)
@@ -108,6 +109,7 @@ class TestUserAddToBasketFromProductPage():
         # check that after opening the page there is no message about the successful addition to the cart
         product_page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         product_page = ProductPage(browser, link_promo)
         product_page.open()
